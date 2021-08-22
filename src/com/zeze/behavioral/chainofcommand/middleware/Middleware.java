790926a72a29e04a31a4af970c02f1f0b69ff941
Zelayan <Lyp82nlf>
2021-08-22 10:38:06 +0800

    feat:add chain of command

---
+++ src/com/zeze/behavioral/chainofcommand/middleware/Middleware.java
@@ -0,0 +1,27 @@
+package com.zeze.behavioral.chainofcommand.middleware;
+
+public abstract class Middleware {
+
+    public Middleware next;
+
+    public Middleware linkWith(Middleware next) {
+        this.next = next;
+        return next;
+    }
+
+    /**
+     * Subclasses will implement this method with concrete checks.
+     */
+    public abstract boolean check(String email, String password);
+
+    /**
+     * Runs check on the next object in chain or ends traversing if we're in
+     * last object in chain.
+     */
+    protected boolean checkNext(String email, String password) {
+        if (next == null) {
+            return true;
+        }
+        return next.check(email, password);
+    }
+}
